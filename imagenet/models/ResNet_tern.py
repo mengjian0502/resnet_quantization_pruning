3d0c9baa48c72544b9fa75cabb61c657e9e717bd
mengjian <jmeng15@asu.edu>
2020-05-09 20:40:22 -0700

    ImageNet training: Full precision shortcut

--- imagenet/models/ResNet_tern.py
+++ imagenet/models/ResNet_tern.py
@@ -259,7 +259,7 @@ class ResNet(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                int_conv2d(self.inplanes, planes * block.expansion,
+                nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
